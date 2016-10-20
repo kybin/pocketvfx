263a5e2faac21d648e069a890baf87b8e2227949
kimhanwoong <khw7096@gmail.com>
2016-10-20 21:46:53 +0900

    사람 자료구조 추가

---
+++ struct.go
@@ -0,0 +1,79 @@
+package main
+
+type Sex int
+
+const (
+	Unknown = iota
+	Male
+	Female
+	Alien
+)
+
+type Body struct {
+	Disk    float64
+	Eye     float64 //입체,VR
+	Ankle   float64
+	Hair    float64
+	Stomach float64
+	Asses   float64 //치질수치
+	Vagina  float64 //질
+	Neck    float64
+	Guts    float64
+	Tongue  float64
+}
+
+type Exp struct {
+	English         float64
+	Career          float64 //경력
+	Programming     float64
+	MacOS           float64
+	Linux           float64
+	Windows         float64
+	Drive           float64
+	Supervizing     float64
+	Marketing       float64
+	Document        float64
+	ProjectManaging float64
+	HumanManaging   float64
+	FinanceManaging float64
+	Previz          float64
+	Matte           float64
+	Concept         float64
+	Render          float64
+	Motion          float64
+	FX              float64
+	Ani             float64
+	Rig             float64
+	Comp            float64
+	Texture         float64
+	Matchmove       float64
+	Modeling        float64
+}
+
+type HiddenExp struct {
+	TendToMove     float64 //역마살. 꼬심에 반응하는수치
+	Patience       float64 //인내심. 빡칠때 사직서를 제출할 확률과 같이 연산할 값.
+	Rational       float64 //합리적인,이성적인
+	Responsibility float64
+	Freindly       float64
+	Lazy           float64
+	LeaderShip     float64
+	Drinking       float64
+	Smoking        float64
+}
+
+type Person struct {
+	Name      string
+	Sex       Sex
+	Body      Body
+	Health    float64 //0 이되면 사망.
+	Stress    float64
+	Exp       Exp
+	HiddenExp HiddenExp //Exp를 같이 사용할지 고민해보기.
+	Layoff    bool
+	Status    Status
+	Cost      int // 만원
+	Position  Position
+	Space     int //지위에 따라.. ?,9,8,6,4,2,1칸
+	Storage   int //주당 사용하는 데이터양.
+}
